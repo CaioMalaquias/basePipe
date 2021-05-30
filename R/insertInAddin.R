b4dd7ad6b8998ecab8e79bf2d2f54fe02c40b966
Caio Malaquias <caio.malaquias@ufpe.br>
2021-05-30 14:56:48 -0300

    teste newbasepipe
    teste

--- R/insertInAddin.R
+++ R/insertInAddin.R
@@ -3,6 +3,6 @@
 #' Call this function as an addin to insert \code{ \%in\% } at the cursor position.
 #'
 #' @export
-insertInAddin <- function() {
-  rstudioapi::insertText(" %in% ")
+basePipeNewlineIndent <- function() {
+  rstudioapi::insertText(" ▷ ")
 }
